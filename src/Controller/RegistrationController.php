891ffbb159832ed89121364bf8ad1f637e7fbd23
virginie <virginie.lenoir17@gmail.com>
2020-12-18 20:49:00 +0100

    AJOUT swift mailer

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,13 +9,15 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Swift_Mailer;
+use Swift_Message;
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/register", name="app_register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, \Swift_Mailer $mailer): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -33,7 +35,25 @@ class RegistrationController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
+            
+
             // do anything else you need here, like send an email
+            $message = (new \Swift_Message('Hello', $user->getNom))
+            ->setFrom('virginielenoir.pro@gmail.com')
+            ->setTo($user->getEmail())
+            ->setBody('Bienvenue sur le site MediaTimes');
+
+            // // you can remove the following code if you don't define a text version for your emails
+            // ->addPart(
+            //     $this->renderView(
+            //         // templates/emails/registration.txt.twig
+            //         'emails/registration.txt.twig',
+            //         ['name' => $name]
+            //     ),
+            //     'text/plain'
+            // )
+            // ;
+            $mailer->send($message);
 
             return $this->redirectToRoute('home');
         }
